0441a47d0cbd962a5b519882ba3330ab4d49e2d5
Amelie Pira <amelie.pira@gmail.com>
2022-05-28 14:17:41 +0100

    added call to fs module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+//requiring inquirer from installed node modules - inquirer will be used to prompt questions and get answers
 const inquirer = require("inquirer");
+
+//requiring fs module from Node.js core - fs will be used to write into the files
+const fs = require("fs");
