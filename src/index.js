f66569494f4eb2b2d1246dbc96d73704b6f86c96
Amelie Pira <amelie.pira@gmail.com>
2022-05-30 21:45:05 +0100

    updated import syntax to es6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //requiring inquirer from installed node modules - inquirer will be used to prompt questions and get answers
-const inquirer = require("inquirer");
+import inquirer from "inquirer";
 
 //requiring fs module from Node.js core - fs will be used to write into the files
-const fs = require("fs");
+import fs from "fs";
