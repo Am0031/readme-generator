48547af080273819802f86b06bf937aa1ce99854
Amelie Pira <amelie.pira@gmail.com>
2022-05-30 22:20:17 +0100

    trialled question syntax

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
 //requiring inquirer from installed node modules - inquirer will be used to prompt questions and get answers
-import inquirer from "inquirer";
+// import inquirer from "inquirer";
+const inquirer = require("inquirer");
 
 //requiring fs module from Node.js core - fs will be used to write into the files
-import fs from "fs";
+// import fs from "fs";
+const fs = require("fs");
+
+const init = async () => {
+  const questions = [
+    {
+      type: "input",
+      name: "title",
+      message: "What's your project title?",
+      validate: (answer) => {
+        if (!answer) {
+          return "Please enter a title for your project";
+        } else {
+          return true;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "userName",
+      message: "What's your github username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What's your email address?",
+    },
+  ];
+
+  const answers = await inquirer.prompt(questions);
+  console.log(answers);
+};
+
+init();
