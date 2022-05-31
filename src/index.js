60723365b264a3cadce15f813c51a9215d810537
Amelie Pira <amelie.pira@gmail.com>
2022-05-31 17:30:59 +0100

    resolved import syntax

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 //requiring inquirer from installed node modules - inquirer will be used to prompt questions and get answers
-// import inquirer from "inquirer";
-const inquirer = require("inquirer");
+import inquirer from "inquirer";
 
 //requiring fs module from Node.js core - fs will be used to write into the files
-// import fs from "fs";
-const fs = require("fs");
+import fs from "fs";
 
 const init = async () => {
   const questions = [
